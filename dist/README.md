7e916b3be5f5828f2bf22a70ad41e15ec1db1dd0
Jaya <jaya.vedantam@gmail.com>
2021-08-22 16:17:45 -0700

    adding license link and section functions

--- dist/README.md
+++ dist/README.md
@@ -1,3 +1,16 @@
-# rfref
-  ![License: MIT](https://img.shields.io/badge/license-Boost1.0-yellowgreen.svg)
+# Read me generator
+  ![License: MIT](https://img.shields.io/badge/license-APACHE2.0-yellowgreen.svg)
 
+  ## Github URL 🌐
+[jaya1983](https://github.com/jaya1983/)
+## Description 📝
+Will genearte a markdown file which will display contents, decscription
+  
+## Table of Contents 🗒
+* [Installations](#dependencies)
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributors](#contributors)
+* [Test](#test)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,17 +9,43 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return (
+      `\n* [License](#license)\n`
+    )
+  }
+  return ''
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `## License 📛
+      Copyright © ${license}. All rights reserved. 
+      
+      Licensed under the ${license} license.`;
+  }
+  return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
   ${renderLicenseBadge(data.license)}
 
+  ## Github URL 🌐
+[${data.github}](https://github.com/${data.github}/)
+## Description 📝
+${data.description}
+  
+## Table of Contents 🗒
+* [Installations](#dependencies)
+* [Usage](#usage)
+${renderLicenseLink(data.license)}
+* [Contributors](#contributors)
+* [Test](#test)
 `;
 }
 
